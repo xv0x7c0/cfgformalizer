38da674649b36e3fa1c01250c84de3255e0604a6
xv0x7c0 <13061441+xv0x7c0@users.noreply.github.com>
2019-03-27 22:37:09 +0100

    initial commit

---
+++ cfgformalizer/stanza.py
@@ -0,0 +1,8 @@
+class Stanza:
+
+  def __init__(self):
+    self.statements = []
+
+
+  def append(self, statement):
+    self.statements.append(statement)
---
+++ cfgformalizer/statement.py
@@ -0,0 +1,10 @@
+import hashlib
+
+class Statement:
+    
+    def __init__(self, lineno, seqnum, context, depth, string):
+      self.lineno = lineno
+      self.seqnum = seqnum
+      self.context = context
+      self.depth = depth
+      self.string = string
